b911eb0f443dda68f3071ced5a5c5b048b4c3ef6
hanolda <hanna.kujawska@yahoo.com>
2018-05-24 19:44:08 +0200

    Add files via upload

--- kMeans.py
+++ kMeans.py
@@ -19,9 +19,13 @@ print('Dataset has {0} columns and {1} rows'.format(col, row))
 # 2. print top 5 lines
 print(wheatData.head(5))
 df = wheatData.values
-   
-data = df[:,[2,7]]
 
+featureX = 0
+featureY = 3
+data = df[:,[featureX,featureY]]
+target = 7
+Y = dF[:, target]
+X = np.array(dF[:, [featureA, featureB]])
  
 
 #data = createClusteredData(100, 5)
@@ -32,9 +36,22 @@ model = KMeans(n_clusters=3)
 model = model.fit(scale(data))
 
 # We can look at the clusters each data point was assigned to
-print(model.labels_)
+print('model.labels_', model.labels_)
 
+
+#dict={str(Kama), str(Rosa), str(Canadian)}
 # And we'll visualize it:
 plt.figure(figsize=(8, 6))
 plt.scatter(data[:,0], data[:,1], c=model.labels_.astype(float))
+plt.title('kMeans scatter plot')
+plt.legend(scatterpoints = 10, loc ='lower right', prpp = dict(size =3)) # to do legend
+plt.xlabel(str(colnames[featureX]))
+plt.ylabel(str(colnames[featureY]))
 plt.show()
+colors = ['navy', 'turquoise', 'darkorange']
+for n, color in enumerate(colors):
+    data = X[Y == n+1]
+    plt.scatter(data[:, 0], data[:, 1], marker='x', color=color)
+    plt.title('kMeans scatter plot')
+    plt.xlabel(str(colnames[featureX]))
+    plt.ylabel(str(colnames[featureY]))
